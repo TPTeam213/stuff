fb8b11b6ba7d1bf0614bb23767f6b7c1c13b3185
TPTeam213 <111410310+TPTeam213@users.noreply.github.com>
2025-04-11 17:50:27 -0400

    Add files via upload

--- Sunday/Sunday_Decrypt.py
+++ Sunday/Sunday_Decrypt.py
@@ -3,11 +3,14 @@ Bruteforce decrypter for my Sunday Cipher, originally conceived for my 4546B cry
 '''
 
 import sys
+import time
 #nltk.download('words') #YOU NEED THIS LINE ONLY IF YOU HAVE NEVER DOWNLOADED NLTK
 from nltk.corpus import words
 eng_words = set(words.words()) #Grabs the english dictionary from nltk and puts in a set for faster searching
 
-
+start_t = 0
+start_t = time.time()
+#I will NOT conform to 0-based indexing
 days = [' sunday', ' monday', ' tuesday', ' wednesday', ' thursday', ' friday', ' saturaday']
 
 
@@ -19,6 +22,8 @@ set2 = set()
 l3 = []
 set3 = set()
 l4 = []
+set4 = set()
+l5 = []
 
 def first_letter():
     #Takes the first digit in the cipher text and grabs the corresponding letter from each day of the week and adds them to a list
@@ -40,6 +45,8 @@ def two_letters():
 def three_letters():
     global raw_output
     two_letters()
+    set1.clear()
+    l2.clear()
     for i in range(7):
         for f in range(len(l1)):
             if (len(days[i]) - 1) >= cyph_list[2]:
@@ -53,6 +60,8 @@ def three_letters():
 def four_letters():
     global raw_output
     three_letters()
+    set2.clear()
+    l3.clear()
     for i in range(7):
         for f in range(len(l2)):
             if (len(days[i]) - 1) >= cyph_list[3]:
@@ -66,6 +75,8 @@ def four_letters():
 def five_letters():
     global raw_output
     four_letters()
+    set3.clear()
+    l4.clear()
     for i in range(7):
         for f in range(len(l3)):
             if (len(days[i]) - 1) >= cyph_list[4]:
@@ -76,15 +87,30 @@ def five_letters():
     raw_output = l4
 
 
+def six_letters():
+    global raw_output
+    five_letters()
+    set4.clear()
+    l5.clear()
+    for i in range(7):
+        for f in range(len(l4)):
+            if (len(days[i]) - 1) >= cyph_list[5]:
+                x = l4[f] + days[i][cyph_list[5]]
+                set4.add(x)
+    for item in set4:
+        l5.append(str(item))
+    raw_output = l5
+
+
 if __name__ == '__main__':
 
 #Input
-    cyph_text = input('Enter cipher text (Two or more numbers, maximum 5): ')
+    cyph_text = input('Enter cipher text (Two or more numbers, maximum 6): ')
 #Converts inputted cipher text into a list of integers for indexing purposes
-    cyph_list = [int(num) for num in cyph_text if num.isdigit()]
+    cyph_list = [int(num) for num in cyph_text]
 
 #Dictionary storing the decode functions based on input length. Functions are called from here using the length of cipher text
-    decode_functions = {2: two_letters, 3: three_letters, 4: four_letters, 5: five_letters}
+    decode_functions = {2: two_letters, 3: three_letters, 4: four_letters, 5: five_letters, 6: six_letters}
 
 #Input validation
     catch = 0
@@ -101,23 +127,32 @@ if __name__ == '__main__':
         print('Your input cannot be blank!')
         catch += 1
     except TypeError:
-        print('Your input must be between 2 and 5 characters. Can you read?')
+        print('Your input must be between 2 and 6 characters. Can you read?')
+        catch += 1
     finally:
         if catch:
+            print('Error encountered. Terminating runtime...')
             sys.exit()
         else:
             #Actual decryption happens here if we pass our exceptions
+            print('Thinking... \n')
             decode_functions.get(len(cyph_text))()
 
+
 #Raw output is written to the raw_output.txt file
-    with open('Sunday/raw_output.txt','w') as file:
+    with open('Sunday/raw_output.txt' , 'w') as file:
+        file.write(f'{cyph_text} can decode into the following:\n')
         file.writelines(entry + '\n' for entry in raw_output)
 
 #Creates a list of clean outputs by checking each entry in the raw output against the english dictionary set from nltk
-        clean_output = [entry for entry in raw_output if entry in eng_words]
+    clean_output = [entry for entry in raw_output if entry in eng_words]
 
 #Writes clean output to the clean_output.txt file
     with open('Sunday/clean_output.txt' , 'w') as file:
+        file.write(f'{cyph_text} can decode into the following:\n')
         file.writelines(entry + '\n' for entry in clean_output)
 
-    print(f'Done! Raw output ({len(raw_output)} possibilities) was written to raw_output.txt and clean output (checked against a dictionary, {len(clean_output)} possible words) was written to clean_output.txt')
+    end_t = 0
+    end_t = time.time()
+    elapsed = end_t - start_t
+    print(f'Done! In {elapsed:.3f} second(s), I calculated {len(raw_output):,} possibilities. This raw output was written to raw_output.txt and clean output (checked against a dictionary, {len(clean_output):,} possible words) was written to clean_output.txt \n')
--- Sunday/Sunday_Encrypt.py
+++ Sunday/Sunday_Encrypt.py
@@ -3,31 +3,34 @@ Program complimentary to Sunday_Decrypt.py. As per the name, this program is an 
 '''
 
 import sys
+import time
 
+#I WILL conform to 0-based indexing
 days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturaday']
-
+start = time.time()
 
 def first_letter():
     first = []
     num = 0
     for i in range(7):
         for f in range(len(days[i])):
-            if days[i][num] == encode_text[0]:
-                first.append(num + 1)
+            if days[i][f] == encode_text[0]:
+                first.append(f + 1)
             num += 1
         num = 0
     return first
 
 
 def second_letter():
+    fast = first_letter()
     global output
     s2 = set()
     num = 0
     for i in range(7):
         for f in range(len(days[i])):
-            if days[i][num] == encode_text[1]:
-                for z in range(len(first_letter())):
-                    s2.add(str(first_letter()[z]) + str((num + 1)))
+            if days[i][f] == encode_text[1]:
+                for z in range(len(fast)):
+                    s2.add(str(fast[z]) + str((f + 1)))
             num += 1
         num = 0
     l2 = [entry for entry in s2]
@@ -36,14 +39,15 @@ def second_letter():
 
 
 def third_letter():
+    fast = second_letter()
     global output
     s3 = set()
     num = 0
     for i in range(7):
         for f in range(len(days[i])):
-            if days[i][num] == encode_text[2]:
-                for z in range(len(second_letter())):
-                    s3.add(str(second_letter()[z]) + str((num + 1)))
+            if days[i][f] == encode_text[2]:
+                for z in range(len(fast)):
+                    s3.add(str(fast[z]) + str((f + 1)))
             num += 1
         num = 0
     l3 = [entry for entry in s3]
@@ -52,14 +56,15 @@ def third_letter():
 
 
 def fourth_letter():
+    fast = third_letter()
     global output
     s4 = set()
     num = 0
     for i in range(7):
         for f in range(len(days[i])):
             if days[i][f] == encode_text[3]:
-                for z in range(len(third_letter())):
-                    s4.add(str(third_letter()[z]) + str((num + 1)))
+                for z in range(len(fast)):
+                    s4.add(str(fast[z]) + str((f + 1)))
             num += 1
         num = 0
     l4 = [entry for entry in s4]
@@ -68,23 +73,40 @@ def fourth_letter():
 
 
 def fifth_letter():
+    fast = fourth_letter()
     global output
     s5 = set()
     num = 0
     for i in range(7):
         for f in range(len(days[i])):
             if days[i][f] == encode_text[4]:
-                for z in range(len(fourth_letter())):
-                    s5.add(str(fourth_letter()[z]) + str((num + 1)))
+                for z in range(len(fast)):
+                    s5.add(str(fast[z]) + str((f + 1)))
             num += 1
         num = 0
     l5 = [entry for entry in s5]
     output = l5
+    return l5
 
+
+def sixth_letter():
+    fast = fifth_letter()
+    global output
+    s6 = set()
+    num = 0
+    for i in range(7):
+        for f in range(len(days[i])):
+            if days[i][f] == encode_text[5]:
+                for z in range(len(fast)):
+                    s6.add(str(fast[z]) + str((f + 1)))
+            num += 1
+        num = 0
+    l6 = [entry for entry in s6]
+    output = l6
 
 if __name__ == '__main__':
 #Get our input, validate it and convert it into a list to be broken down by encryption functions
-    encoders = {2: second_letter, 3: third_letter, 4:fourth_letter, 5:fifth_letter}
+    encoders = {2: second_letter, 3: third_letter, 4:fourth_letter, 5:fifth_letter, 6:sixth_letter}
     encode_text = input('Enter text to be encoded: ')
     keeper = encode_text
     encode_text = list(encode_text)
@@ -97,7 +119,6 @@ if __name__ == '__main__':
     catch = 0
     try:
         x = 1 / len(encode_text)
-        encoders.get(len(encode_text))()
     except ZeroDivisionError:
         print('Your input cannot be blank!')
         catch += 1
@@ -109,12 +130,16 @@ if __name__ == '__main__':
             sys.exit()
         else:
             #Actual encryption happens here if we pass our exceptions
+            print('Thinking...')
             encoders.get(len(encode_text))()
 
 #All possible number combinations that correctly encode the inputted word are stored to combinations.txt
     with open('Sunday/combinations.txt','w') as file:
+        file.write(f'{keeper} can be encoded as the following:\n')
         file.writelines(entry + '\n' for entry in output)
 
 #Stats on how many possible combinations were created for the given word and where they were written is displayed
-    print(f'There are {len(output)} possible combinations to encode "{keeper}"!')
+    end = time.time()
+    full = end - start
+    print(f'In {full:.3f} second(s), I calculated {len(output)} possible combinations to encode "{keeper}"!')
     print(f'All possible combinations to encode "{keeper}" were calculated and saved to combinations.txt')
